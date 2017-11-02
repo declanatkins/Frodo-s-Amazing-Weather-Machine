a73ac6301eedae189d3253f654c63dd5ddf05724
Declan <declanatkins@gmail.com>
2017-11-02 13:42:21 +0000

    Added keys for Eventbrite and Ticketmaster apps

---
+++ src/service/events/eventbrite/EventbriteSearch.java
@@ -0,0 +1,7 @@
+package service.events.eventbrite;
+
+public class EventbriteSearch {
+	
+	private static final String KEY = "ZRVXYET5HJBPTCEHZN";
+
+}
---
+++ src/service/events/ticketmaster/TicketMasterSearch.java
@@ -0,0 +1,6 @@
+package service.events.ticketmaster;
+
+public class TicketMasterSearch {
+
+	private static final String API_KEY = "lLShZiKoxGxMSjEJSNtzCXZFTEGgscGZ";
+}
