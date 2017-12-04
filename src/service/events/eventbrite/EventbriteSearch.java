af236dfda1d0555f4477636faf1f52e1df3f391b
Declan <declanatkins@gmail.com>
2017-12-04 23:06:33 +0000

    Did some reorganizing and added jar file for SQLite for database

--- src/service/events/eventbrite/EventbriteSearch.java
+++ src/service/events/eventbrite/EventbriteSearch.java
@@ -3,5 +3,7 @@ package service.events.eventbrite;
 public class EventbriteSearch {
 	
 	private static final String KEY = "ZRVXYET5HJBPTCEHZN";
+	
+	
 
 }
--- src/service/events/ticketmaster/TicketMasterSearch.java
+++ src/service/events/ticketmaster/TicketMasterSearch.java
@@ -3,4 +3,7 @@ package service.events.ticketmaster;
 public class TicketMasterSearch {
 
 	private static final String API_KEY = "lLShZiKoxGxMSjEJSNtzCXZFTEGgscGZ";
+	private static final String OAUTH_TOKEN = "MROBYYM22SNUAIVWGPWB";
+	
+	
 }
