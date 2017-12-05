d5ebca1881418893dc8402369dd30b400dddde9f
Declan <declanatkins@gmail.com>
2017-12-05 16:32:03 +0000

    Fixed query for creating tables and added insert for events search

---
+++ src/service/events/EventSearch.java
@@ -0,0 +1,5 @@
+package service.events;
+
+public class EventSearch {
+
+}
--- src/service/users/DatabaseHandler.java
+++ src/service/users/DatabaseHandler.java
@@ -2,6 +2,7 @@ package service.users;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -18,19 +19,53 @@ public class DatabaseHandler {
 		}
 		
 		try {
+			//create the tables if they do not exist
 			Statement s = conn.createStatement();
-			String query = "CREATE TABLE IF NOT EXISTS users (\n" +
-						"id INTEGER PRIMARY KEY,\n" +
+			String queryUsers =//user table 
+						"CREATE TABLE IF NOT EXISTS users (\n" +
+						"id INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
 						"user_name TEXT NOT NULL,\n" +
 						"password TEXT NOT NULL\n);";
-			s.execute(query);
+			String queryEvents = //events table
+						"CREATE TABLE IF NOT EXISTS events (\n" +
+						"id INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+						"userID INTEGER NOT NULL,\n" +
+						"eventID INTEGER NOT NULL,\n" +
+						"source TEXT NOT NULL\n);";
+			String querySearches =//events searches
+						"CREATE TABLE IF NOT EXISTS event_searches (\n" +
+						"id INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+						"userID INTEGER NOT NULL,\n" +
+						"params TEXT NOT NULL\n);";
+			s.execute(queryUsers);
+			s.execute(queryEvents);
+			s.execute(querySearches);
 		} catch (SQLException e) {
 			System.err.println("Error verifying tables");
+		}
+	}
+	
+	public boolean insertSearch(String searchParams, int userID) {
+		try {
+			String query = 
+						"INSERT INTO event_searches "+
+						"(userID, params)\n" +
+						"VALUES (?,?);";
+			PreparedStatement prepState = conn.prepareStatement(query);
+			prepState.setString(2, searchParams);
+			prepState.setInt(1, userID);
+			prepState.execute();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.err.println("Error inserting search");
+			return false;
 		}
 	}
 	
 	
 	public static void main(String[] args) {
 		DatabaseHandler dbh = new DatabaseHandler();
+		dbh.insertSearch("test", 1);
 	}
 }
