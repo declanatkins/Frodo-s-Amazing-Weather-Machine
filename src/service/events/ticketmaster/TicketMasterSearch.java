f47961890ea7cf38ed2379b9356f9f2c2a3da355
Declan <declanatkins@gmail.com>
2017-12-08 06:36:02 +0000

    started simple front end

--- src/service/events/ticketmaster/TicketMasterSearch.java
+++ src/service/events/ticketmaster/TicketMasterSearch.java
@@ -1,9 +1,11 @@
 package service.events.ticketmaster;
 
+import com.google.gson.Gson;
+
 public class TicketMasterSearch {
 
 	private static final String API_KEY = "lLShZiKoxGxMSjEJSNtzCXZFTEGgscGZ";
 	private static final String OAUTH_TOKEN = "MROBYYM22SNUAIVWGPWB";
-	
+	private Gson gson;
 	
 }
